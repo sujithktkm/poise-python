ac979fbcb9ec0807d0ef5785a7ec8cb46905d73c
Sujith Katakam <sujith@clari.com>
2017-11-29 11:24:23 -0800

    fix version number

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'nobody@poise_python.com'
 license          'All rights reserved'
 description      'Poise python cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '1.6.0'
