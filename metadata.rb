fc3b63a6b7ca4af0d7b9f4b62b2965b52bb551b3
Sujith Katakam <sujith@clari.com>
2017-11-29 09:06:17 -0800

    Add metadata.rb file for poise-python CB

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'poise_python'
+maintainer       'Poise python'
+maintainer_email 'nobody@poise_python.com'
+license          'All rights reserved'
+description      'Poise python cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
